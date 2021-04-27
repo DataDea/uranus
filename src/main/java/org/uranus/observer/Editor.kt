d4be01200dd35259145ae3b75ec34545ab5f5ef4
hai.yang <hai.yang@17zuoye.com>
2021-04-27 10:57:55 +0800

    fix

---
+++ src/main/java/org/uranus/observer/Editor.kt
@@ -0,0 +1,9 @@
+package org.uranus.observer
+
+/**
+ * @author yanghai
+ * @since 2021/4/27-10:39 上午
+ */
+open class Editor {
+
+}
---
+++ src/main/java/org/uranus/observer/EventListener.kt
@@ -0,0 +1,8 @@
+package org.uranus.observer
+
+/**
+ * @author yanghai
+ * @since 2021/4/27-10:47 上午
+ */
+interface EventListener {
+}
