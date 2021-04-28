929349c3e19285dd534619060507b40be5885a2f
hai.yang <hai.yang@17zuoye.com>
2021-04-28 15:48:10 +0800

    fix

---
+++ src/test/java/org/uranus/observer/TestObserver.java
@@ -0,0 +1,13 @@
+package org.uranus.observer;
+
+/**
+ * @author yanghai
+ * @since 2021/4/28-3:43 下午
+ */
+public class TestObserver {
+
+    public void test() {
+
+    }
+
+}
---
+++ src/test/java/org/uranus/single/TestSingle.java
@@ -0,0 +1,8 @@
+package org.uranus.single;
+
+/**
+ * @author yanghai
+ * @since 2021/4/28-3:43 下午
+ */
+public class TestSingle {
+}
