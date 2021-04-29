1b3d7e99a4c6be75c6a421c1e81767620c05f7d1
hai.yang <hai.yang@17zuoye.com>
2021-04-29 10:26:10 +0800

    fix

--- src/test/java/org/uranus/observer/TestObserver.java
+++ src/test/java/org/uranus/observer/TestObserver.java
@@ -19,7 +19,7 @@ public class TestObserver {
         GoblinTeacherEvent goblinTeacherEvent = new GoblinTeacherEvent();
         goblinTeacherEvent.setEventId(String.valueOf(RandomUtils.nextLong(0L, 1000000000000L)));
         goblinTeacherEvent.setType("TEACHER_CLASS");
-        goblinTeacherEvent.setEventTime(DateUtils.parseDate("2020-04-27 23:59:59"));
+        goblinTeacherEvent.setEventTime(DateUtils.parseDate("2020-04-27 23:59:59", "yyyy-MM-dd HH:mm:dd"));
         goblinTeacherEvent.setName("英语课");
         goblinTeacherEvent.setAddress("阶梯102");
         goblinTeacherEvent.setClassz("网络1402");
@@ -29,7 +29,7 @@ public class TestObserver {
         GoblinStudentEvent goblinStudentEvent = new GoblinStudentEvent();
         goblinStudentEvent.setEventId(String.valueOf(RandomUtils.nextLong(0L, 1000000000000L)));
         goblinStudentEvent.setType("STUDENT_CLASS");
-        goblinStudentEvent.setEventTime(DateUtils.parseDate("2020-04-27 23:59:59"));
+        goblinStudentEvent.setEventTime(DateUtils.parseDate("2020-04-27 23:59:59", "yyyy-MM-dd HH:mm:dd"));
         goblinStudentEvent.setName("英语课");
         goblinStudentEvent.setSubject("ENGLISH");
         goblinStudentEvent.setTeacher("张老师");
